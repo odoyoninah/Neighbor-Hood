a4848542a3e2944933015c5827d07f2586af0d4f
odoyonina <odoyoninah@gmail.com>
2022-06-19 18:10:59 +0300

    create url pattern for home page

--- hood/urls.py
+++ hood/urls.py
@@ -5,3 +5,13 @@ from . import views
 from django.conf.urls.static import static
 
 
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('account/', include('django.contrib.auth.urls')), 
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
