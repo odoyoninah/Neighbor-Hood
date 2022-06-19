72c1babed1436f45a9873321e9d95bdab8971e1f
odoyonina <odoyoninah@gmail.com>
2022-06-19 18:08:40 +0300

    create signup and login form

---
+++ hood/urls.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+from django.urls import path,include
+from django.contrib import admin
+from . import views
+from django.conf.urls.static import static
+
+
--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+import email
+from multiprocessing import context
+from django.forms import Form
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
+#handle all the status code responses.
 
-# Create your views here.
+
+
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+
+   
+
+        return redirect('login')
+        
+    else:    
+        form = RegisterForm()
+    return render(request,'registration/signup.html',{'form':form})
+
+
+    
+def logout(request):
+    logout(request)
+    messages.success(request,'You have successfully logged out!')
+    return redirect('index')
+
