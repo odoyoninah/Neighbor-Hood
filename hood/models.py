8643d6ea3c6fda384559854cd3b19a5dcfd5a196
odoyonina <odoyoninah@gmail.com>
2022-06-19 23:59:11 +0300

    create business class

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from django.db import models
 
 # Create your models here.
@@ -14,6 +15,17 @@ class Hood(models.Model):
     description = models.TextField()
     admin = models.ForeignKey('Admin', on_delete=models.CASCADE)
 
+    def create_hood(self):
+        self.save()
+        
+    def save_hood(self):
+        self.save()
+
+    def delete_hood(self):
+        self.delete()
+
+    def update_hood(self):
+        self.save()
 
     def __str__(self):
         return self.name
@@ -27,6 +39,38 @@ class Business(models.Model):
 
     def __str__(self):
         return self.name
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+    admin = models.ForeignKey('Admin', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Admin(models.Model):
+    admin_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    post = models.TextField()
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    hood = models.ForeignKey('Hood', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts/')
+
+    def __str__(self):
+        return self.title
+
+
 
 
 
