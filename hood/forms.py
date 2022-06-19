09234f8f96b80d2157a43c32a85ccbb685ae2607
odoyonina <odoyoninah@gmail.com>
2022-06-20 00:53:55 +0300

    import forms from models class

--- hood/forms.py
+++ hood/forms.py
@@ -2,10 +2,11 @@ from django import forms
 from dataclasses import fields
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,Hood,Business,Post
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import *
 
 
 class RegisterForm(UserCreationForm):
@@ -14,4 +15,14 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+class BusinessForm(ModelForm):
+    class Meta:
+        model = Business
+        fields = ['name', 'description', 'location', 'category', 'image']
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'description', 'image']
 
--- hood/views.py
+++ hood/views.py
@@ -6,12 +6,49 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from .forms import RegisterForm
+
+from hood.models import Post
+from .forms import *
 from django.contrib.auth.decorators import login_required
 #handle all the status code responses.
 
 def index(request):
+    post = Post.objects.all()
     return render(request,'index.html')
+
+def business(request):
+    return render(request,'business.html')
+
+def hood(request):
+    return render(request,'hood.html')
+
+@login_required(login_url='/accounts/login/')
+def createpost(request):
+    if request.method=='POST':
+        form = PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+
+            return redirect('hood')
+    else:
+        form = PostForm()
+    return render(request,'createpost.html',{'form':form}) 
+
+@login_required(login_url='/accounts/login/')
+def createbusiness(request):
+    if request.method=='POST':
+        form = BusinessForm(request.POST,request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+
+            return redirect('business')
+    else:
+        form = BusinessForm()
+    return render(request,'createbusiness.html',{'form':form}) 
+
+
 
 
 
