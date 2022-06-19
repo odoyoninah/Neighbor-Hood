20f6dbf74eef7de69d7ea34ea8f321471ba9d601
odoyonina <odoyoninah@gmail.com>
2022-06-20 00:16:49 +0300

    create superuser

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Hood, Business, Admin
 
-# Register your models here.
+admin.site.register(Hood)
+admin.site.register(Business)
+admin.site.register(Admin)
+
+
--- hood/urls.py
+++ hood/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('account/', include('django.contrib.auth.urls')), 
+    
 ]
 
 if settings.DEBUG:
